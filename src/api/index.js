3af48b80067b2dd00ebf196f3e40af1022583635
omkar jadhav <omkar1jadhav0@gmail.com>
2025-03-06 17:43:12 +0530

    env added

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,11 @@
-const baseUrl = "https://api.weatherapi.com/v1/current.json?key= 01f749ae4f2c4a02bc365257251301";
 export const getWeatherDataForCity = async (city)=>{
-    const response = await fetch(`${baseUrl}&q=${city}&aqi=yes`)
+    const response = await fetch(`${process.env.BASE_URL}&q=${city}&aqi=yes`)
     return await response.json();
 
 }
 
 export const getWeatherDataForLocation = async (lat, lon)=>{
-    const response = await fetch(`${baseUrl}&q=${lat},${lon}&aqi=yes`)
+    const response = await fetch(`${process.env.BASE_URL}&q=${lat},${lon}&aqi=yes`)
     return await response.json();
 
 }
